182b4a9f7a5e071b647460951f51e022dd5ecd99
babaygt <yigitingbaba@gmail.com>
2024-08-06 16:46:17 +0200

    Update info component

--- app/components/home/Info.jsx
+++ app/components/home/Info.jsx
@@ -1,12 +1,12 @@
 const Info = () => {
 	return (
 		<div className='home-info'>
-			<h1 className='home-title'>Yigit Baba</h1>
+			<h1 className='home-title'>Yiğit Baba</h1>
 			<h3 className='home-subtitle'>Full Stack Software Developer</h3>
 			<p className='home-description'>
-				Hi, I&apos;m Yiğit, a passionate Developer. I approach challenges with a
-				scientific mindset and love creating projects and products that solve
-				real-world problems.
+				Hi, I&apos;m Yiğit, a passionate Developer based in Berlin. I approach
+				challenges with a scientific mindset and love creating projects and
+				products that solve real-world problems.
 			</p>
 		</div>
 	)
