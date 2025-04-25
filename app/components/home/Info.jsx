06426baee7ac4dd963303ec89c2ce259e1fc829b
babaygt <yigit.baba@code.berlin>
2025-04-26 00:38:21 +0200

    Update personal info and styles to reflect current role

--- app/components/home/Info.jsx
+++ app/components/home/Info.jsx
@@ -1,14 +1,16 @@
 const Info = () => {
-	return (
-		<div className='home-info'>
-			<h1 className='home-title'>Yiğit Baba</h1>
-			<h3 className='home-subtitle'>Full Stack Software Developer</h3>
-			<p className='home-description'>
-				Hi, I&apos;m Yiğit, a passionate Developer based in Berlin. I approach
-				challenges with a scientific mindset and love creating projects and
-				products that solve real-world problems.
-			</p>
-		</div>
-	)
-}
-export default Info
+  return (
+    <div className="home-info">
+      <h1 className="home-title">Yiğit Baba</h1>
+      <h3 className="home-subtitle">Full Stack Software Developer</h3>
+      <p className="home-description">
+        Hi, I&apos;m Yiğit, a passionate Developer based in Berlin, currently
+        working as a Working Student Full Stack Software Developer at SAP,
+        focusing on Applied AI Topics. I approach challenges with a scientific
+        mindset and love building projects and products that solve real-world
+        problems.
+      </p>
+    </div>
+  );
+};
+export default Info;
