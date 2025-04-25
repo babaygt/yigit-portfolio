a56b1fe8eb99edadafa92208a17375bb0af6d0e4
babaygt <yigit.baba@code.berlin>
2025-04-26 00:49:27 +0200

    Add email icon and link to contact via mail

--- app/components/home/Social.jsx
+++ app/components/home/Social.jsx
@@ -1,20 +1,28 @@
-import Link from 'next/link'
-import { FaGithub, FaLinkedin } from 'react-icons/fa'
+import Link from "next/link";
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 const Social = () => {
-	return (
-		<div className='home-social'>
-			<Link href='https://github.com/babaygt' className='home-social-icon'>
-				<FaGithub />
-			</Link>
+  return (
+    <div className="home-social">
+      <Link href="https://github.com/babaygt" className="home-social-icon">
+        <FaGithub />
+      </Link>
 
-			<Link
-				href='https://www.linkedin.com/in/yigitbaba/'
-				className='home-social-icon'
-			>
-				<FaLinkedin />
-			</Link>
-		</div>
-	)
-}
-export default Social
+      <Link
+        href="https://www.linkedin.com/in/yigitbaba/"
+        className="home-social-icon"
+      >
+        <FaLinkedin />
+      </Link>
+
+      <Link
+        href="mailto:yigitbaba.contact@gmail.com"
+        target="_blank"
+        className="home-social-icon"
+      >
+        <FaEnvelope />
+      </Link>
+    </div>
+  );
+};
+export default Social;
